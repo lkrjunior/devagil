670016672b8a54dac7b29ac929513bab801e15f2
DIEGO BRAUN DA CUNHA <ALU201830331@uniritter.local>
2018-09-28 20:26:03 -0300

    [DEV]Fixed travis CI

--- src/main/java/com/makeryourevent/main/MainClass.java
+++ src/main/java/com/makeryourevent/main/MainClass.java
@@ -1,8 +1,14 @@
 package com.makeryourevent.main;
 
+import com.makeryourevent.repository.EventRepository;
+
 public class MainClass {
 
     public static void main(String args[]) {
         System.out.print("Hello World");
+
+        EventRepository repository = new EventRepository();
+
+        System.out.print(repository.getListOfEvents().size());
     }
 }
